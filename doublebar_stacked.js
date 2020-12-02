1dc17f2fb733d6fd99e86879030cde507dee7615
Catherine Beimfohr <cjbeimfohr13@gmail.com>
2020-12-02 11:07:55 -0500

    separation of sports for each year

--- doublebar_stacked.js
+++ doublebar_stacked.js
@@ -146,14 +146,36 @@
     
      
     const olympics_1984=data.filter(d=>d.year==="1984");
+    var sport_1984=olympics_1984.map(data=>data.sport)
+    console.log(sport_1984)
+
     const olympics_1988=data.filter(d=>d.year==="1988");
+    var sport_1988=olympics_1988.map(data=>data.sport)
+    console.log(sport_1988)
+
     const olympics_1992=data.filter(d=>d.year==="1992");
+    var sport_1992=olympics_1992.map(data=>data.sport)
+    console.log(sport_1992)
+
     const olympics_1996=data.filter(d=>d.year==="1996");
-    const olympics_2000=data.filter(d=>d.year==="2000");
-    const olympics_2004=data.filter(d=>d.year==="2004");
-    const olympics_2008=data.filter(d=>d.year==="2008");
-    const olympics_2016=data.filter(d=>d.year==="2016");
+    var sport_1996=olympics_1996.map(data=>data.sport)
+    console.log(sport_1996)
     
+    const olympics_2000=data.filter(d=>d.year==="2000");
+    var sport_2000=olympics_2000.map(data=>data.sport)
+    console.log(sport_2000)
+
+    const olympics_2004=data.filter(d=>d.year==="2004");
+    var sport_2004=olympics_2004.map(data=>data.sport)
+    console.log(sport_2004)
+
+    const olympics_2008=data.filter(d=>d.year==="2008");
+    var sport_2008=olympics_2008.map(data=>data.sport)
+    console.log(sport_2008)
+
+    const olympics_2016=data.filter(d=>d.year==="2016");
+    var sport_2016=olympics_2016.map(data=>data.sport)
+    console.log(sport_2016)
 
     const menOlympics2016=olympics_2016.filter(d=>d.sex==="male");
     const womenOlympics2016=olympics_2016.filter(d=>d.sex==="female");
