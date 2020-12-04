b4623c2db16f38bf8317d34cfa38563d7d5c79f3
Catherine Beimfohr <cjbeimfohr13@gmail.com>
2020-12-03 19:27:36 -0500

    web structure

--- doublebar_stacked.js
+++ doublebar_stacked.js
@@ -525,45 +525,45 @@
     arrayYear2016("I yam what I yam and always will be what I yam");
 
 
-//     // Initializes the page with a default plot
-//     function init() {
-//       dog = [{
-//       x: sportEventsAll,
-//       y: sportEventCountAll}];
+    // Initializes the page with a default plot
+    function init() {
+      dog = [{
+      x: sportEventsAll,
+      y: sportEventCountAll}];
 
-//       Plotly.newPlot("plot", dog);
-//       }
+      Plotly.newPlot("plot", dog);
+      }
 
-//     // Call updatePlotly() when a change takes place to the DOM
-//       d3.selectAll("#selDataset").on("change", updatePlotly);
+    // Call updatePlotly() when a change takes place to the DOM
+      d3.selectAll("#selDataset").on("change", updatePlotly);
 
-//     // This function is called when a dropdown menu item is selected
-//     function updatePlotly() {
-//     // Use D3 to select the dropdown menu
-//     var dropdownMenu = d3.select("#selDataset");
-//     // Assign the value of the dropdown menu option to a variable
-//     var dataset = dropdownMenu.property("value");
+    // This function is called when a dropdown menu item is selected
+    function updatePlotly() {
+    // Use D3 to select the dropdown menu
+    var dropdownMenu = d3.select("#selDataset");
+    // Assign the value of the dropdown menu option to a variable
+    var dataset = dropdownMenu.property("value");
 
-//     // Initialize x and y arrays
-//     var x = [];
-//     var y = [];
+    // Initialize x and y arrays
+    var x = [];
+    var y = [];
 
-//     if (dataset === '1976') {
-//     x = sportEvent1976
-//     y = sportEventCount1976;
-//    }
+    if (dataset === '1976') {
+    x = sportEvent1976
+    y = sportEventCount1976;
+   }
 
-//     if (dataset === '1984') {
-//     x = sportEvent1984;
-//     y = sportEventCount1984;
-//    }
+    if (dataset === '1984') {
+    x = sportEvent1984;
+    y = sportEventCount1984;
+   }
 
-//   // Note the extra brackets around 'x' and 'y'
-//       Plotly.restyle("plot", "x", [x]);
-//       Plotly.restyle("plot", "y", [y]);
-//     }
+  // Note the extra brackets around 'x' and 'y'
+      Plotly.restyle("plot", "x", [x]);
+      Plotly.restyle("plot", "y", [y]);
+    }
 
-// init();
+init();
 
 
 
--- practice.js
+++ practice.js
@@ -1,19 +1,19 @@
 
-     //   function yearsUnique(year){
-  //     var uniqueYears = [];
+       function yearsUnique(year){
+      var uniqueYears = [];
       
-  //     // Loop through array values
-  //     for(i=0; i < year.length; i++){
-  //         if(uniqueYears.indexOf(year[i]) === -1) {
-  //           uniqueYears.push(year[i]);
-  //         }
-  //     }
-  //     return uniqueYears;
+      // Loop through array values
+      for(i=0; i < year.length; i++){
+          if(uniqueYears.indexOf(year[i]) === -1) {
+            uniqueYears.push(year[i]);
+          }
+      }
+      return uniqueYears;
 
-  // }
-  // var years = data.map(data=>data.year)
-  // var singleYears = yearsUnique(years);
-  // console.log(singleYears);
+  }
+  var years = data.map(data=>data.year)
+  var singleYears = yearsUnique(years);
+  console.log(singleYears);
 
 //   function sportsUnique(event){
 //     var uniqueSports = [];
