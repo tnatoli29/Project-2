b7a29e83a3e06e660b79f7ace8e3c2ec1128d9ab
Thomas <tpnats@gmail.com>
2020-12-04 16:47:56 -0500

    2nd commit, dropdown menu added and worked on getting data to change with dropdown select

--- pie-chart/plots.js
+++ pie-chart/plots.js
@@ -1,28 +1,49 @@
  // Part 5 - Working Pie Chart
+function init() {
+  //Grab the dropdown select element
+  var dropdown = d3.select("#selDataset");
 
-d3.csv("../Resources/olympics.csv").then(function(data) {
-  //console.log(data[0]);
-  //console.log(data[0].sex)
+  d3.csv("../Resources/olympics.csv").then(function(data) {
+    //console.log(data[0]);
+    var gender = data.map(d => d.sex);
+    //console.log(gender)
+
+    var years = data.map(d => d.year);
+    var uniYears = [...new Set(years)];
+    //console.log(uniYears);
+
+    uniYears.forEach((sample) => {
+      dropdown
+        .append("option")
+        .text(sample)
+        .property("value", sample);
+    });
+    //Build the charts with the first sample
+    var firstSample = uniYears[0];
+    buildCharts(firstSample);
+    });
+  }
+
+function buildCharts(sample) {
+// Construct pie chart
+  d3.csv("../Resources/olympics.csv").then((data) => {
+    var years = data.map(d => d.year);
+    var uniYears = [...new Set(years)];
+
+    var male = data.filter(d => d.sex === "male").length
+    //console.log(male)
+
+    var female = data.filter(d => d.sex === "female").length;
+    //console.log(female)
 
 
- function genderCount(obj) {
-    return obj.reduce((acc, data) => {
-        if (data.sex === 'male') {
-            acc.male++;
-        }
-        else {
-            acc.female++;
-        }
-        return acc;
-    }, {male: 0, female: 0});
-}
-console.log(genderCount(data))
 
 
  var trace1 = {
    labels: ["Male", "Female"],
-   values: [553, 482],
-   type: 'pie'
+   values: [male, female],
+   type: 'pie',
+   textinfo: "label+percent"
  };
 
  var data = [trace1];
@@ -34,3 +55,12 @@ console.log(genderCount(data))
 Plotly.newPlot("plot", data, layout);
 
 });
+}
+function optionChanged(newSample) {
+  // Change data in charts and graphs based on selected Subject ID
+  buildCharts(newSample);
+}
+
+
+init();
+
