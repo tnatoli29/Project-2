24f68ea298aa502cd8a9689aa094aa7cc6208d5d
crismariano <cris.mariano6@gmail.com>
2020-12-05 19:31:54 -0500

    commit 25

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
-
 import csv
 from flask import Flask, render_template, json, current_app as app
 import os
-# static/data/test_data.json
 
-csvFilePath = "Resources/olympics.csv"
-jsonFilePath = "Resources/olympics.json"
+
+# File path for read csv file and write to json file
+csvFilePath = "Resources/usaGoldMedals.csv"
+jsonFilePath = "Resources/usaGoldMedals.json"
 
 # Read the csv and add the data to a dictionary
-
 data = {}
+
 with open(csvFilePath) as csvFile:
     csvReader = csv.DictReader(csvFile)
     for csvRow in csvReader:
-        id = csvRow["id"]
-        data[id] = csvRow
+        year = csvRow["year"]
+        data[year] = csvRow
 
 with open(jsonFilePath, "w") as jsonFile:
     jsonFile.write(json.dumps(data, indent=4))     
