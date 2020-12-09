6657250c0e3fbee53fbc810262430df1fa12f9e5
Erika Yi <erikayidesign@gmail.com>
2020-12-09 12:52:27 -0500

    Erika JS Work

---
+++ static/ey_js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1Ijoia2lraTAyMDIiLCJhIjoiY2tocnVlaWw4MDh6MDJzcXdzeDIwaTVoZyJ9.-MVQjXXNEB3hiPn5UaoIog";
---
+++ static/ey_js/ey_coordinates.js
@@ -0,0 +1,30 @@
+// Coordinate for each cities 
+// 2016 – Rio, Brazil
+// 22.9068° S, 43.1729° W
+
+// 2008 – Beijing, China
+// 39.9042° N, 116.4074° E
+
+// 2004 – Athens, Greece
+// 37.9838° N, 23.7275° E
+
+// 2000 – Sydney, Australia  
+// 33.8688° S, 151.2093° E
+
+// 1996 – Atlanta, USA
+// 33.7490° N, 84.3880° W
+
+// 1992- Barcelona, Spain
+// 41.3851° N, 2.1734° E
+
+// 1988- Seoul, South Korea
+// 37.5665° N, 126.9780° E
+
+// 1984- Los Angeles, USA
+// 34.0522° N, 118.2437° W
+
+// 1980- Moscow, Russia
+// 55.7558° N, 37.6173° E
+
+// 1976- Montreal, Canada
+// 45.5017° N, 73.5673° W
