e2e8a2b221142d66c7555fc19abe0c56f6f51d7f
Thomas <tpnats@gmail.com>
2020-12-08 19:05:12 -0500

    8th commit, finished piechart and made it responsive

--- pie-chart/plots.js
+++ pie-chart/plots.js
@@ -1,8 +1,8 @@
-//// function buildPlot(){
+
 d3.csv("../Resources/olympics.csv").then(function(data) {
    //console.log(data);
 
-
+//filter the results to only view results by specific year
 var olympics_1976=data.filter(d=>d.year==="1976");
 var olympics_1984=data.filter(d=>d.year==="1984");
 var olympics_1988=data.filter(d=>d.year==="1988");
@@ -14,6 +14,7 @@ var olympics_2008=data.filter(d=>d.year==="2008");
 var olympics_2016=data.filter(d=>d.year==="2016");
 //console.log(olympics_2008)
 
+// filter the results even more to only pull male and female from the data
 var male1976 = olympics_1976.filter(d => d.sex === "male")
 var female1976 = olympics_1976.filter(d => d.sex === "female")
 var male1984 = olympics_1984.filter(d => d.sex === "male")
@@ -35,7 +36,7 @@ var female2016 = olympics_2016.filter(d => d.sex === "female")
 //console.log(male1976)
 
 
-// create 2 data_set
+// Get the number of male and female athletes
 var olympics1976 = [male1976.length, female1976.length];
 var olympics1984 = [male1984.length, female1984.length];
 var olympics1988 = [male1988.length, female1988.length];
@@ -50,11 +51,16 @@ var olympics2016 = [male2016.length, female2016.length];
 
 var labels = ["Male", "Female"]
 
+
 function init() {
   var data = [{
     values: olympics1976,
     labels: labels,
+    marker:{
+        colors: ["#193050", "#e62e00"]
+     },
     type: "pie"
+
   }];
 
     var layout = {
@@ -64,35 +70,58 @@ function init() {
 
  Plotly.newPlot("pie", data, layout);
 }
-// On change to the DOM, call getData()
 d3.selectAll("#selDataset").on("change", getData);
 
 // Function called by DOM changes
 function getData() {
+
+    // On change to the DOM, call getData()
+
   var dropdownMenu = d3.select("#selDataset");
+  var dataNumbers = dropdownMenu.property("value");
   // Assign the value of the dropdown menu option to a variable
 
   // Initialize an empty array for the country's data
   var data = [];
 
-  if (data == 'olympics1976') {
+  if (dataNumbers == 'olympics1976') {
       data = olympics1976;
   }
-  else if (data == 'olympics1984') {
+  else if (dataNumbers == 'olympics1984') {
       data = olympics1984;
   }
-  else if (data == 'olympics1988') {
+  else if (dataNumbers == 'olympics1988') {
       data = olympics1988;
+  }
+  else if (dataNumbers == 'olympics1992') {
+      data = olympics1992;
+  }
+  else if (dataNumbers == 'olympics1996') {
+      data = olympics1996;
+  }
+  else if (dataNumbers == 'olympics2000') {
+      data = olympics2000;
+  }
+  else if (dataNumbers == 'olympics2004') {
+      data = olympics2004;
+  }
+  else if (dataNumbers == 'olympics2008') {
+      data = olympics2008;
+  }
+  else if (dataNumbers == 'olympics2016') {
+      data = olympics2016;
   }
   // Call function to update the chart
   updatePlotly(data);
 }
 // Update the restyled plot's values
 function updatePlotly(newdata) {
-  Plotly.restyle("pie", "values", [newdata]);
+  Plotly.restyle("pie", "values",[newdata]);
 }
 
+console.log(getData)
 init();
+
 });
 
 
